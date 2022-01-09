8831a29f2f1221227e60c41d73e0d14ce3dfefeb
Shawn Maida <shawn.maida@fostermade.co>
2022-01-09 13:47:15 -0500

    Updating language

--- system/user/addons/cartthrob_shipping_fedex/language/english/cartthrob_shipping_fedex_lang.php
+++ system/user/addons/cartthrob_shipping_fedex/language/english/cartthrob_shipping_fedex_lang.php
@@ -3,20 +3,15 @@
 $lang = [
         'fedex_short_title' => 'FedEx Live Rates',
         'fedex_overview' => '
-            <p>This shipping plugin requires  CartThrob 5+, PHP 7.1+, and the Soap module enabled. Settings for this add-on are managed with the plugins <a href="/admin.php?/cp/addons/settings/cartthrob_live_rates_fedex">Settings Page</a>.</p>
-
+            <p>This shipping plugin requires  CartThrob 6+, PHP 7.4+, and the PHP Soap module.</p>
             <h3 style="display:block">Use of This Plugin</h3>
-
             <p>LiveRates shipping costs are not updated automatically when the cart contents are modified. LiveRates shipping plugins requires that you manually request a shipping quote at some point during your checkout process. If you do not use the <a href="https://cartthrob.com/docs/tags_detail/get_shipping_options/index.html">{exp:cartthrob:get_shipping_options}</a> tag, your shipping costs may not be set for checkout.</p> 
-                
-                <p>For more details about this and other live rates plugins see: <a href="https://cartthrob.com/docs/tags_detail/get_live_rates_form/">http://cartthrob.com/docs/tags_detail/get_live_rates_form/</a></p>
-
+            <p>For more details about this and other live rates plugins see: <a href="https://cartthrob.com/docs/tags_detail/get_live_rates_form/">http://cartthrob.com/docs/tags_detail/get_live_rates_form/</a></p>
             <h3 style="display:block">Estimate Accuracy Warning</h3>
             <p>If your actual packing and shipping methods differ from the information you use to request the cost estimate, your shipping costs may vary from the estimated value. By default the entire cart weight is used to calculate shipping costs, along with default length, width, and height values set below.</p>
-
             <p>Each time items in the cart are added, updated, or removed, shipping costs are reset to zero when using a LiveRates shipping plugin. It is recommended that you check to see that shipping costs are set before allowing a customer to check out. For example: {if "{exp:cartthrob:cart_shipping prefix=""}" = "0.00"}show live rates{if:else}show checkout{/if}</p>
-
-            <h3 style="display:block">Dimensional Weight Warning</h3><p>Length + Width + Height values are <strong>required</strong> for shipping quotes using LiveRates plugins. Some shipping methods such as overnight, next-day, and two-day shipping methods may only calculate costs by box dimensions rather than standard weights. Please consult documentation provided by your shipping company, or your shipping representative for more information.</p>
+            <h3 style="display:block">Dimensional Weight Warning</h3>
+            <p>Length + Width + Height values are <strong>required</strong> for shipping quotes using Live Rates plugins. Some shipping methods such as overnight, next-day, and two-day shipping methods may only calculate costs by box dimensions rather than standard weights. Please consult documentation provided by your shipping company, or your shipping representative for more information.</p>
         ',
         'fedex_2_day' => 'Domestic - 2 Day',
         'fedex_api_key' => 'API Key',
